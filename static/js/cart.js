457323ae37937f7a5c052765a6ba42aca2c85209
saikatsingha <saikatsingha@gmail.com>
2021-10-09 21:33:52 +0530

    Add to cart complete

--- static/js/cart.js
+++ static/js/cart.js
@@ -10,22 +10,27 @@ for(var i = 0; i < updateBtns.length; i++) {
         if (user == 'AnonymousUser'){
             console.log('User is not Authenticated')
         } else {
-            console.log('User is Authenticated, sending data...')
+            updateUserOrder(productId, action)
         }
 
     })
 }
 
 function updateUserOrder(productId, action){
+    console.log('User is logged in, sending Data...')
+
     var url = '/update_item/'
-    console.log('URL:', url)
+    
     fetch(url, {
-        method:'POST',
+        method:'POST', 
         headers:{
-            'content-Type':'application/json',
-            'X-CSRFToken':csrftoken,
-        },
-        body:JSON.stringify({'productId': productId, action:'action'})
+            'Content-Type':'application/json', 
+            'X-CSRFToken':csrftoken
+        }, 
+        body:JSON.stringify({
+            'productId': productId, 'action':action
+        }
+        )
     })
 
     .then((response) => {
@@ -34,5 +39,6 @@ function updateUserOrder(productId, action){
 
     .then((data) => {
         console.log('data:',data)
+        location.reload()
     })
 }
--- store/views.py
+++ store/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 def store(request):
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+
+    else:
+        items = []
+        order = {'get_cart_items':0, 'get_cart_total':0}
+        cartItems = order['get_cart_items']
+
     products = Product.objects.all()
-    context = {'products': products}
+    context = {'products': products, 'cartItems': cartItems}
     return render(request, 'store/store.html', context)    
 
 def cart(request):
@@ -14,13 +28,18 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
 
     else:
         items = []
         order = {'get_cart_items':0, 'get_cart_total':0}
+        cartItems = order['get_cart_items']
 
-    context = {'items':items, 'order':order}
+
+    context = {'items':items, 'order':order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
+
+
 
 def checkout(request):
     if request.user.is_authenticated:
@@ -39,8 +58,7 @@ def updateItem(request):
     data = json.loads(request.body)
     productId = data['productId']
     action = data['action']
-    print('Action', action)
-    print('productId', productId)
+    
 
     customer = request.user.customer
     product = Product.objects.get(id=productId)
@@ -49,13 +67,32 @@ def updateItem(request):
     orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
 
     if action == 'add':
+        print(action)
         orderItem.quantity = (orderItem.quantity + 1)
+        
     elif action == 'remove':
         orderItem.quantity = (orderItem.quantity - 1)
-    
+           
     orderItem.save()
 
     if orderItem.quantity <= 0:
         orderItem.delete()
 
     return JsonResponse('Item was added', safe=False)
+
+@csrf_exempt
+def processOrder(request):
+    transction_id = datetime.datetime.now().timestamp()
+    data = json.loads(reqiest.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = Order.objects.get_or_create(customer = customer, complete=False)
+
+
+    else:
+        items = []
+        order = {'get_cart_items':0, 'get_cart_total':0}
+
+    context = {'items':items, 'order':order}
