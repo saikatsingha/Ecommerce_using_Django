4e1f7b64dde57416246469b229eb32c2274117c3
saikatsingha <saikatsingha@gmail.com>
2021-10-10 14:50:33 +0530

    Implementing user signup after item added to cart

--- store/views.py
+++ store/views.py
@@ -46,12 +46,13 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
-
+        cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_items':0, 'get_cart_total':0}
+        order = {'get_cart_items':0, 'get_cart_total':0,'shipping': False}
+        cartItems = order['get_cart_items']
 
-    context = {'items':items, 'order':order}
+    context = {'items':items, 'order':order, 'cartItems': cartItems}
     return render(request, 'store/checkout.html', context)
 
 def updateItem(request):
