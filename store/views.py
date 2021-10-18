63df846aef556a99224aff01be443fc9de2c5541
saikatsingha <saikatsingha@gmail.com>
2021-10-18 23:31:11 +0530

    Stripe Done only dummy

--- store/views.py
+++ store/views.py
@@ -1,13 +1,16 @@
 from django.db.models.deletion import Collector
+from django.conf import settings
 from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
 import json
 import datetime 
 from . utils import cookieCart, cartData, guestOrder
+import stripe
 
 
 # Create your views here.
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 def store(request):
 
@@ -24,19 +27,22 @@ def cart(request):
     cartItems = data['cartItems']
     order = data['order']
     items = data['items']
+    
 
     context = {'items':items, 'order':order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
+    #print(request.method)
     data = cartData(request)
 
     cartItems = data['cartItems']
     order = data['order']
     items = data['items']
+    key = settings.STRIPE_PUBLISHABLE_KEY
 
-    context = {'items':items, 'order':order, 'cartItems': cartItems}
+    context = {'items':items, 'order':order, 'cartItems': cartItems, 'key': key}
     return render(request, 'store/checkout.html', context)
 
 def updateItem(request):
@@ -52,7 +58,7 @@ def updateItem(request):
     orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
 
     if action == 'add':
-        print(action)
+        # print(action)
         orderItem.quantity = (orderItem.quantity + 1)
         
     elif action == 'remove':
@@ -64,6 +70,17 @@ def updateItem(request):
         orderItem.delete()
 
     return JsonResponse('Item was added', safe=False)
+
+def thankYou(request):
+    if request.method == 'POST':
+        print(request.POST)
+        charge = stripe.Charge.create(
+            amount =  500,
+            currency = 'INR',
+            description = 'Cart Value in Stripe',
+            source = request.POST['stripeToken']
+        )
+    return render(request, 'store/thankyou.html')   
 
 # from django.views.decorators.csrf import csrf_exempt
 # @csrf_exempt
